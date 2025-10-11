41a617934abfeddd7f1ae30cd7d1131037403983
sychic <47618543+Sychic@users.noreply.github.com>
2025-10-11 00:58:30 -0400

    feat: custom coord input

--- src/portal.ts
+++ src/portal.ts
@@ -1,27 +1,39 @@
 import { atom, computed } from "nanostores";
 
-export type Coords = { x: number; z: number };
-export const coords = atom<Coords | undefined>(undefined);
+export type Coord = { value: number, dimension: Dimension }
+export const xCoord = atom<Coord | undefined>(undefined);
+export const zCoord = atom<Coord | undefined>(undefined);
 
 export enum Dimension {
     Overworld = "overworld",
     Nether = "the_nether"
 }
-export const dimension = atom<Dimension | undefined>(undefined);
 
-export const convertedCoords = computed(
-    [coords, dimension],
-    ($coords, $dimension) => {
-        if (!$coords || $dimension === undefined) return undefined;
-        if ($dimension === Dimension.Overworld) {
-            return { x: Math.floor($coords.x / 8), z: Math.floor($coords.z / 8) };
+export const convertedXCoord = computed(
+    [xCoord],
+    ($xCoord) => {
+        if (!$xCoord) return undefined;
+        if ($xCoord.dimension === Dimension.Overworld) {
+            return Math.floor($xCoord.value / 8);
         } else {
-            return { x: Math.floor($coords.x * 8), z: Math.floor($coords.z * 8) };
+            return Math.floor($xCoord.value * 8);
         }
     }
-);
+)
 
-const TELEPORT_REGEX = /^\/execute in minecraft:(?<dimension>\w+) run tp @s (?<x>-?\d+\.?\d{2}) -?\d+\.?\d{2} (?<z>-?\d+\.?\d{2}) -?\d+\.?\d{2} -?\d+\.?\d{2}$/;
+export const convertedZCoord = computed(
+    [zCoord],
+    ($zCoord) => {
+        if (!$zCoord) return undefined;
+        if ($zCoord.dimension === Dimension.Overworld) {
+            return Math.floor($zCoord.value / 8);
+        } else {
+            return Math.floor($zCoord.value * 8);
+        }
+    }
+)
+
+export const TELEPORT_REGEX = /^\/execute in minecraft:(?<dimension>\w+) run tp @s (?<x>-?\d+\.?\d{2}) -?\d+\.?\d{2} (?<z>-?\d+\.?\d{2}) -?\d+\.?\d{2} -?\d+\.?\d{2}$/;
 
 export const parseCoords = (text: string) => {
     const match = text.match(TELEPORT_REGEX);
